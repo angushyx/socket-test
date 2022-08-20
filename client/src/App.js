3a3af380fe0845292465b41010d56aab81af79a3
Angus <94235117+angushyx@users.noreply.github.com>
2022-08-20 13:36:24 +0800

    init

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+import Alert from "react-bootstrap/Alert";
+
+const App = () => {
+  return (
+    <>
+      <>
+        {[
+          "primary",
+          "secondary",
+          "success",
+          "danger",
+          "warning",
+          "info",
+          "light",
+          "dark",
+        ].map((variant) => (
+          <Alert key={variant} variant={variant}>
+            This is a {variant} alert—check it out!
+          </Alert>
+        ))}
+      </>
+      <div></div>
+      <Button>123</Button>
+      <div className="btn btn-info">App</div>
+    </>
+  );
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const { Server } = require("socket.io");
+var cors = require("cors");
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["GET", "POST"],
+  },
+});
+
+server.listen(PORT, console.log(`Server has successfully Start at: ${PORT}`));
