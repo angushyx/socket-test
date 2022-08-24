530a04c62811a8a83285a659a513ae0d8703c251
Angus <sickmi14798@gmail.com>
2022-08-24 11:52:11 +0800

    need to fix contact logic

--- server/index.js
+++ server/index.js
@@ -29,30 +29,47 @@ const io = new Server(server, {
 //   res.json(data)
 // })
 
+// !TEST 1
+// io.on('connection', (socket) => {
+//   //測試有沒有連線成功
+//   const id = socket.handshake.query.id
+
+//   console.log(`User Connected:${socket.id}`)
+
+//   /**
+//    * 建造room：
+//    */
+//   socket.on('join_room', (data) => {
+//     console.log('room num', data)
+//     socket.join(data)
+//   })
+
+//   /**
+//    * 建立溝通連線：
+//    * 1. on、 emit API 搭配使用 ： 第一個參數要跟彼此溝通的通道，名稱要一致。
+//    * 例如這是一條send_message 的連線， 在後端這邊的 on 第一個參數寫send_message
+//    * 前端的 emit 參數就也要寫一樣。
+//    */
+//   socket.on('send_message', (data) => {
+//     //  broadcast：全伺服器都會接受到
+//     // socket.broadcast.emit('receive_message', data)
+//     //  to：只有特定伺服器可以接收到，前面要鮮血條件，鏈式寫法
+//     console.log('data.room', data.room)
+//     socket.to(data.room).emit('receive_message', data)
+//   })
+// })
+
 io.on('connection', (socket) => {
-  //測試有沒有連線成功
-  console.log(`User Connected:${socket.id}`)
+  const id = socket.handshake.query.id
+  socket.join(id)
 
-  /**
-   * 建造room：
-   */
-  socket.on('join_room', (data) => {
-    console.log('room num', data)
-    socket.join(data)
-  })
-
-  /**
-   * 建立溝通連線：
-   * 1. on、 emit API 搭配使用 ： 第一個參數要跟彼此溝通的通道，名稱要一致。
-   * 例如這是一條send_message 的連線， 在後端這邊的 on 第一個參數寫send_message
-   * 前端的 emit 參數就也要寫一樣。
-   */
-  socket.on('send_message', (data) => {
-    //  broadcast：全伺服器都會接受到
-    // socket.broadcast.emit('receive_message', data)
-    //  to：只有特定伺服器可以接收到，前面要鮮血條件，鏈式寫法
-    console.log('data.room', data.room)
-    socket.to(data.room).emit('receive_message', data)
+  socket.on('send-message', ({ recipients, text }) => {
+    recipients.forEach((recipient) => {
+      console.log(recipients)
+      const newRecipients = recipients.filter((r) => r !== recipient)
+      newRecipients.push(id)
+      socket.broadcast.to(recipient).emit('receive-message', { recipients: newRecipients, sender: id, text })
+    })
   })
 })
 
